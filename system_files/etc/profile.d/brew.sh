7aeb21f41524d071e7a92592ae808d3f1fccc3e3
Tulip Blossom <tulilirockz@outlook.com>
2025-12-17 11:38:05 -0300

    Update system_files/etc/profile.d/brew.sh
    Co-authored-by: renner <80410025+renner0e@users.noreply.github.com>

--- system_files/etc/profile.d/brew.sh
+++ system_files/etc/profile.d/brew.sh
@@ -4,7 +4,7 @@ if [[ -d /home/linuxbrew/.linuxbrew && $- == *i* ]] ; then
   export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar"
   export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew"
   [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}"
-  export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
+  export INFOPATH="${INFOPATH}:/home/linuxbrew/.linuxbrew/share/info"
   export PATH="${PATH}:/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin"
   if [ "$(basename $(readlink /proc/$$/exe))" == "zsh" ] ; then
     fpath[1,0]="/home/linuxbrew/.linuxbrew/share/zsh/site-functions"
