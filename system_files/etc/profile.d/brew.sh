72f27fa0dc81572d27a8483197bdcb88a56640e8
Alberto García <4411661+GarciaLnk@users.noreply.github.com>
2025-12-18 16:06:18 +0100

    chore: use `grep -E` instead of `egrep`

--- system_files/etc/profile.d/brew.sh
+++ system_files/etc/profile.d/brew.sh
@@ -3,6 +3,6 @@
 # Prioritize system binaries to prevent brew overriding things like dbus
 # See: https://github.com/ublue-os/brew/blob/54b30cc07d3211fca65ca5cc724e9812c8c79b77/system_files/usr/lib/systemd/system/brew-upgrade.service#L17-L22
 if [[ -d /home/linuxbrew/.linuxbrew && $- == *i* ]] ; then
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv | egrep -v '\bPATH=')"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv | grep -Ev '\bPATH=')"
   export PATH="${PATH}:${HOMEBREW_PREFIX}/bin:${HOMEBREW_PREFIX}/sbin"
 fi
